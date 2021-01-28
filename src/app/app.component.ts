fa8934e3d35224913088871047462416a60f5ba8
ebedoyaalzate <esteban.bedoya.ext@kavak.com>
2021-01-28 17:09:55 -0500

    add art work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import DataBase from './dataBase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'da-vinci';
+export class AppComponent implements OnInit {
+  pictures = DataBase
+
+  ngOnInit(): void {
+    const leftImg = document.getElementById('left-img') as HTMLObjectElement;
+    const title = document.getElementById('title') as HTMLObjectElement;
+
+    window.addEventListener('scroll', () => {
+      const { scrollY } = window;
+      leftImg.style.transform = `translateX(${scrollY * -0.9}px)`;
+      title.style.transform = `translate3d(${scrollY * 0.5}px, ${scrollY * -0.8}px, 0)`;
+    });
+
+  }
 }
---
+++ src/app/dataBase.ts
@@ -0,0 +1,34 @@
+const daVinciPicture = [
+  {
+    name: 'The Last Supper',
+    description: 'The Last Supper (Italian: Il Cenacolo [il tʃeˈnaːkolo] or L`Ultima Cena is a late 15th-century mural painting by Italian artist Leonardo da Vinci housed by the refectory of the Convent of Santa Maria delle Grazie in Milan, Italy. It is one of the Western world`s most recognizable paintings.',
+    image: 'https://d32dm0rphc51dk.cloudfront.net/jI0TpkGaAoJ9JL4s4JPdBg/large.jpg'
+  },
+  {
+    name: 'Mona Lisa',
+    description: 'The Mona Lisa or La Gioconda is a half-length portrait painting by Italian artist Leonardo da Vinci. Considered an archetypal masterpiece of the Italian Renaissance, it has been described as "the best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world". The painting`s novel qualities include the subject`s enigmatic expression, the monumentality of the composition, the subtle modelling of forms, and the atmospheric illusionism.',
+    image: 'https://d32dm0rphc51dk.cloudfront.net/5L1xjKC_und1uiKCpUPHhw/large.jpg'
+  },
+  {
+    name: 'Virgin of the Rocks',
+    description: 'The Virgin of the Rocks (Italian: Vergine delle rocce; sometimes the Madonna of the Rocks) is the name of two paintings by the Italian artist Leonardo da Vinci, of the same subject, with a composition which is identical except for several significant details. The version generally considered the prime version, the earlier of the two, is unrestored and hangs in the Louvre in Paris. The other, which was restored between 2008–2010, hangs in the National Gallery, London.',
+    image: 'https://d32dm0rphc51dk.cloudfront.net/Jv-e1fhDjg61OYhhsMoiQg/large.jpg'
+  },
+  {
+    name: 'Ginevra de` Benci',
+    description: 'Ginevra de` Benci is a portrait painting by Leonardo da Vinci of the 15th-century Florentine aristocrat Ginevra de` Benci (born c.  1458) The oil-on-wood portrait was acquired by the National Gallery of Art in Washington, D.C. in 1967. The sum of US$5 million—an absolute record price at the time—came from the Ailsa Mellon Bruce Fund and was paid to the Princely Family of Liechtenstein. It is the only painting by Leonardo on public view in the Americas.',
+    image: 'https://d32dm0rphc51dk.cloudfront.net/3qC7QFuWQL2m2hKGIbGnPw/large.jpg'
+  },
+  {
+    name: 'Saint John the Baptist',
+    description: 'Saint John the Baptist is a High Renaissance oil painting on walnut wood by Leonardo da Vinci. Likely completed between 1513 to 1516, it is believed to be his final painting. The original size of the painting was 69 by 57 centimetres (27 in × 22 in). It is now exhibited at the Musée du Louvre in Paris, France.',
+    image: 'https://d32dm0rphc51dk.cloudfront.net/aDr2R-YDhsQl303KVMLc3g/large.jpg'
+  },
+  {
+    name: 'Wreath of Laurel',
+    description: 'Ginevra de` Benci`s portrait is two-sided. This is the back, a portrait of Ginevra. A scroll bears her Latin motto, meaning "Beauty Adorns Virtue." In the center, a sprig of juniper (in Italian, ginepro) suggests Ginevra`s name, while the encircling laurel and palm symbolize her intellectual and moral virtue. The laurel also happened to be the personal emblem of Bernardo Bembo, Venetian ambassador to Florence, whose deep and abiding relationship with Ginevra is revealed in poems dedicated to them.',
+    image: 'https://d32dm0rphc51dk.cloudfront.net/MFeji-IefT4x6n4LiZKBlg/large.jpg'
+  },
+]
+
+export default daVinciPicture
